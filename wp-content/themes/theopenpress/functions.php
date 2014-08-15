15cb4ef0978acfd30cbf5c9bf9a4c3f7e1922ee0
wleklinski <brian@rootsrated.com>
2014-08-15 16:49:10 -0400

    Remove unused scripts from enqueue

--- wp-content/themes/theopenpress/functions.php
+++ wp-content/themes/theopenpress/functions.php
@@ -96,10 +96,6 @@ function tOP_scripts() {
 
 	wp_enqueue_style( 'tOP-app', get_template_directory_uri() . '/assets/css/app.css' );
 
-	wp_enqueue_script( 'tOP-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'tOP-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
