62feade55006e177837f22701b4431016f5d4545
wleklinski <brian@rootsrated.com>
2014-08-15 16:47:47 -0400

    Initial app.css commit

--- wp-content/themes/theopenpress/functions.php
+++ wp-content/themes/theopenpress/functions.php
@@ -94,6 +94,8 @@ add_action( 'widgets_init', 'tOP_widgets_init' );
 function tOP_scripts() {
 	wp_enqueue_style( 'tOP-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'tOP-app', get_template_directory_uri() . '/assets/css/app.css' );
+
 	wp_enqueue_script( 'tOP-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'tOP-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
