e61d6363838c8d1558fd790fe1994df67e975bc7
wleklinski <brian@rootsrated.com>
2014-08-17 12:36:10 -0400

    Add favicon to site & admin

--- wp-content/themes/theopenpress/functions.php
+++ wp-content/themes/theopenpress/functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+  $content_width = 640; /* pixels */
 }
 
 if ( ! function_exists( 'tOP_setup' ) ) :
@@ -22,50 +22,50 @@ if ( ! function_exists( 'tOP_setup' ) ) :
  */
 function tOP_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on tOP, use a find and replace
-	 * to change 'tOP' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'tOP', get_template_directory() . '/languages' );
+  /*
+   * Make theme available for translation.
+   * Translations can be filed in the /languages/ directory.
+   * If you're building a theme based on tOP, use a find and replace
+   * to change 'tOP' to the name of your theme in all the template files
+   */
+  load_theme_textdomain( 'tOP', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+  // Add default posts and comments RSS feed links to head.
+  add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+  /*
+   * Enable support for Post Thumbnails on posts and pages.
+   *
+   * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+   */
+  //add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'tOP' ),
-	) );
+  // This theme uses wp_nav_menu() in one location.
+  register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'tOP' ),
+  ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
+  /*
+   * Switch default core markup for search form, comment form, and comments
+   * to output valid HTML5.
+   */
+  add_theme_support( 'html5', array(
+    'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+  ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link'
-	) );
+  /*
+   * Enable support for Post Formats.
+   * See http://codex.wordpress.org/Post_Formats
+   */
+  add_theme_support( 'post-formats', array(
+    'aside', 'image', 'video', 'quote', 'link'
+  ) );
 
-	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'tOP_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+  // Setup the WordPress core custom background feature.
+  add_theme_support( 'custom-background', apply_filters( 'tOP_custom_background_args', array(
+    'default-color' => 'ffffff',
+    'default-image' => '',
+  ) ) );
 }
 endif; // tOP_setup
 add_action( 'after_setup_theme', 'tOP_setup' );
@@ -76,15 +76,15 @@ add_action( 'after_setup_theme', 'tOP_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function tOP_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'tOP' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+  register_sidebar( array(
+    'name'          => __( 'Sidebar', 'tOP' ),
+    'id'            => 'sidebar-1',
+    'description'   => '',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ) );
 }
 add_action( 'widgets_init', 'tOP_widgets_init' );
 
@@ -92,13 +92,13 @@ add_action( 'widgets_init', 'tOP_widgets_init' );
  * Enqueue scripts and styles.
  */
 function tOP_scripts() {
-	wp_enqueue_style( 'tOP-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'tOP-style', get_stylesheet_uri() );
 
-	wp_enqueue_style( 'tOP-app', get_template_directory_uri() . '/assets/css/app.css' );
+  wp_enqueue_style( 'tOP-app', get_template_directory_uri() . '/assets/css/app.css' );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'tOP_scripts' );
 
@@ -126,3 +126,21 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/*
+ * Add Favicon
+ */
+function tOP_favicon() {
+  echo '<link rel="shortcut icon" href="' . get_bloginfo('url') . '/favicon.ico" />';
+}
+add_action('wp_head', 'tOP_favicon');
+
+
+/*
+ * Add Admin Favicon
+ */
+function tOP_admin_favicon() {
+  echo '<link rel="shortcut icon" href="' . get_bloginfo('url') . '/favicon-admin.ico" />';
+}
+add_action('login_head', 'tOP_admin_favicon');
+add_action('admin_head', 'tOP_admin_favicon');
